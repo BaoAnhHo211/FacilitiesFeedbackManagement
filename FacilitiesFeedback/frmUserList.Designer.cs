13b2848b61d4d85b1dd0a3b0007ce112f115d743
BaoAnhHo211 <baoanhho.211@gmail.com>
2022-08-17 07:42:42 +0700

    Add project files.

---
+++ FacilitiesFeedback/frmUserList.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace FacilitiesFeedback
+{
+    partial class frmUserList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvUser = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new FacilitiesFeedback.UIControl.CustomizeButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDelete = new FacilitiesFeedback.UIControl.CustomizeButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvUser
+            // 
+            this.dgvUser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUser.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvUser.BackgroundColor = System.Drawing.Color.White;
+            this.dgvUser.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUser.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUser.Location = new System.Drawing.Point(12, 68);
+            this.dgvUser.Name = "dgvUser";
+            this.dgvUser.RowHeadersWidth = 51;
+            this.dgvUser.RowTemplate.Height = 29;
+            this.dgvUser.Size = new System.Drawing.Size(1002, 469);
+            this.dgvUser.TabIndex = 0;
+            this.dgvUser.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUser_CellClick);
+            this.dgvUser.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUser_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(343, 52);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(175)))), ((int)(((byte)(71)))));
+            this.btnSearch.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(175)))), ((int)(((byte)(71)))));
+            this.btnSearch.BorderColor = System.Drawing.Color.Black;
+            this.btnSearch.BorderRadius = 10;
+            this.btnSearch.BorderSize = 2;
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(231, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(97, 39);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.TextColor = System.Drawing.Color.White;
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Status"});
+            this.comboBox1.Location = new System.Drawing.Point(2, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(742, 46);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "* Status: 0 = Active, 1 = Inactive \r\n* Role: 0 = User, 1 = Administrator\'s Manage" +
+    "r, 2 = Administrator\'s Staff, 3 = Engineer Staff";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.BackColor = System.Drawing.Color.Brown;
+            this.btnDelete.BackgroundColor = System.Drawing.Color.Brown;
+            this.btnDelete.BorderColor = System.Drawing.Color.Black;
+            this.btnDelete.BorderRadius = 4;
+            this.btnDelete.BorderSize = 2;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(917, 17);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 39);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.TextColor = System.Drawing.Color.White;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmUserList
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(239)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(1026, 590);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmUserList";
+            this.Text = "frmMemberList";
+            this.Load += new System.EventHandler(this.frmUserList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUser)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUser;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private UIControl.CustomizeButton btnSearch;
+        private System.Windows.Forms.Label label1;
+        private UIControl.CustomizeButton btnDelete;
+    }
+}
---
+++ FacilitiesFeedback/frmUserList.cs
@@ -0,0 +1,75 @@
+﻿using BusinessObject.Models;
+using DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace FacilitiesFeedback
+{
+    public partial class frmUserList : Form
+    {
+        IUserRepository userRepository = new UserRepository();
+        public User currentUser { get; set; }
+        List<User> userList = new List<User>();
+        int CurrentRow;
+        public frmUserList()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUserList_Load(object sender, System.EventArgs e)
+        {
+            ShowData();
+        }
+
+        private void ShowData()
+        {
+            userList = (List<User>)userRepository.GetUsers();
+            dgvUser.DataSource = null;
+            dgvUser.DataSource = userList;
+            dgvUser.Columns["FeedbackStaffs"].Visible = false;
+            dgvUser.Columns["FeedbackUsers"].Visible = false;
+        }
+
+        private void dgvUser_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            frmUserDetail frmUserDetail = new frmUserDetail
+            {
+                user = (User)dgvUser.Rows[e.RowIndex].DataBoundItem,
+                isAdmin = true,
+            };
+            if (frmUserDetail.ShowDialog() == DialogResult.OK)
+            {
+                ShowData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var user = (User)dgvUser.Rows[CurrentRow].DataBoundItem;
+                if (user.Id == currentUser.Id)
+                {
+                    throw new Exception("This is your account! Cannot Delete");
+                }
+                var Confirm = MessageBox.Show("Are you want to delete this User?", "Confirm Delete!!!", MessageBoxButtons.YesNo);
+                if (Confirm == DialogResult.Yes)
+                {
+                    userRepository.DeleteUser(user);
+                    MessageBox.Show("Delete Successful", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ShowData();
+                }
+            }
+            catch (Exception ex)
+            {
+                new frmMessageBox(ex.Message).ShowDialog();
+            }
+        }
+
+        private void dgvUser_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            CurrentRow = e.RowIndex;
+        }
+    }
+}
